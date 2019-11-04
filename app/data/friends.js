765ced58e252800bf12a71ca03e9329248ad26ba
JackRyan1989 <jack.jackryan@gmail.com>
2019-11-03 22:20:07 -0500

    Added external JS files and some HTML formatting.

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,37 +4,37 @@ let friends =
         {
             name: 'Jebediah',
             photo: 'https://cdn.acidcow.com/pics/20110919/amish_mugshots_07.jpg',
-            scores: [5,2,6,7,4,3,7,9,8,1]
+            scores: [5,2,4,3,4,3,1,2,5,1]
         },
         {
             name: 'Hezekiah',
             photo: 'https://piximus.net/media2/48906/amish-mug-shots-2.jpg',
-            scores: [1,1,2,5,4,8,6,3,9,0]
+            scores: [1,1,2,5,4,3,2,3,2,2]
         },
         {
             name: 'Ezekial',
             photo: 'https://cdn.acidcow.com/pics/20110919/amish_mugshots_01.jpg',
-            scores: [8,9,4,5,4,4,2,2,7,0]
+            scores: [1,2,3,5,4,4,2,2,4,5]
         },
         {
             name: 'Herschel',
             photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Julia_Margaret_Cameron_-_John_Herschel_%28Metropolitan_Museum_of_Art_copy%2C_restored%29.jpg/1200px-Julia_Margaret_Cameron_-_John_Herschel_%28Metropolitan_Museum_of_Art_copy%2C_restored%29.jpg',
-            scores: [6,6,5,4,7,2,1,2,2,4]
+            scores: [4,3,5,4,2,2,1,2,2,1]
         },
         {
             name: 'Bartholomew',
             photo: 'http://catholicphilly.com/media-files/2012/08/patriarch-Bartholomew1.jpg',
-            scores: [0,9,5,6,1,2,7,5,6,0]
+            scores: [1,2,5,2,1,2,3,5,4,3]
         },
         {
             name: 'Erick Estrada',
             photo: 'https://i.ytimg.com/vi/pYN1aUDkabM/maxresdefault.jpg',
-            scores: [6,3,9,6,2,4,7,8,1,5]
+            scores: [4,3,4,3,2,4,2,3,1,5]
         },
         {
             name: 'Urkel',
             photo: 'https://i.ytimg.com/vi/8FIujwA8xXg/maxresdefault.jpg',
-            scores: [5,4,5,8,1,2,0,8,5,9]
+            scores: [5,4,5,4,1,2,2,3,5,3]
         }
     ];
 
---
+++ app/js/survey.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    let qArr = ['How much do you like lobster?', 'Which number are you?', 'How many horse and buggies do you own?', 'Please choose the number six(6).', 'How many steps did you walk up today?', 'How many toes do you have on your left foot?', 'How old were you when Norm MacDonald was born?', 'How many times did you think about ants today?', 'How many stars would you rate Hulk Hogan?', 'If you could be any cheese in the world, which number would it be?'];
+    let optArr = [1, 2, 3, 4, 5];
+    let opt = $("<option>");
+    let select = $("<select>").addClass("form-control");
+    let label = $("<label>");
+
+
+    function makeQs() {
+        for (let i = 0; i < qArr.length; i++){
+            
+        }
+    };
+
+
+    makeQs();
+
+
+
+});
