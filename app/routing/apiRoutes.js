8c9d598159aaf91338f02520730a7677fbb46acd
JackRyan1989 <jack.jackryan@gmail.com>
2019-11-04 11:04:41 -0500

    Friend calculation almost done.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,8 +12,24 @@ apiRouter.get('/friends', function (req, res) {
 
 apiRouter.post('/friends', function (req, res) {
     //Logic for friend matching here
-    let answers = req.body;
-    console.log(answers);
+    let name = req.body.name;
+    let answers = req.body.scores;
+    let friendVal = [];
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    for (let i = 0; i < answers.length; i++) {
+        answers[i] = parseInt(answers[i]);
+    }
+    for (let i = 0; i < friends.length; i++) {
+        let subArr = answers.map(function(item, index){
+           return Math.abs(item - friends[i].scores[index]);
+        });
+        friendVal.push(subArr.reduce(reducer));
+        console.log(subArr.reduce(reducer));
+        console.log(friendVal);
+     }
+     let myScore = answers.reduce(reducer); 
+     console.log(myScore);
 });
 
 module.exports = apiRouter;
+
