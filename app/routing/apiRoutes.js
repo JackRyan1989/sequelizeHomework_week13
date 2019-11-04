551be187e0d56f0f61832fd8d1c51c34f7392562
JackRyan1989 <jack.jackryan@gmail.com>
2019-11-04 18:12:05 -0500

    Friend finding algorithm complete.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,20 +15,25 @@ apiRouter.post('/friends', function (req, res) {
     let name = req.body.name;
     let answers = req.body.scores;
     let friendVal = [];
+    let smallest = 50;
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     for (let i = 0; i < answers.length; i++) {
         answers[i] = parseInt(answers[i]);
     }
     for (let i = 0; i < friends.length; i++) {
-        let subArr = answers.map(function(item, index){
-           return Math.abs(item - friends[i].scores[index]);
-        });
-        friendVal.push(subArr.reduce(reducer));
-        console.log(subArr.reduce(reducer));
-        console.log(friendVal);
+        friendVal.push(friends[i].scores.reduce(reducer));
      }
      let myScore = answers.reduce(reducer); 
-     console.log(myScore);
+     let diffArr = friendVal.map(function(item, index){
+        return Math.abs(item - myScore);
+     });
+     for (let i = 0; i < diffArr.length; i++) {
+         if (diffArr[i] < smallest) {
+            smallest = diffArr[i];
+         }
+     } 
+     foundFriend = diffArr.indexOf(smallest);
+     myFriend = friends[foundFriend];
 });
 
 module.exports = apiRouter;
